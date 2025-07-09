13cdae063cb0e36f95875cccd927c1ed5c56b1a5
MoxLotus <brianblake1@live.com>
2025-07-09 00:54:19 -0600

    Added basic elements.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,46 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Wrapture
+namespace Wrapture;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void BrowseSource_Click(object sender, RoutedEventArgs e)
+    {
+        using var dialog = new FolderBrowserDialog();
+        if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
         {
-            InitializeComponent();
+            SourceFolderTextBox.Text = dialog.SelectedPath;
+
+            // Load files and update ListBox
+            var files = Directory.GetFiles(dialog.SelectedPath);
+            SourceFilesListBox.ItemsSource = files.Select(System.IO.Path.GetFileName).Where(name => !string.IsNullOrEmpty(name) && name.EndsWith(".mkv"));
+        }
+    }
+
+    private void BrowseDestination_Click(object sender, RoutedEventArgs e)
+    {
+        using var dialog = new FolderBrowserDialog();
+        if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        {
+            DestinationFolderTextBox.Text = dialog.SelectedPath;
         }
     }
 }
